3079da4cc5805416a238c4b150edb122a4eb3baa
Fichtelcoder <de.frankenapps@gmail.com>
2025-07-13 09:57:41 +0200

    Use NDK 29.0.13599879.

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.markosyan.flutter_zxing_example"
     compileSdk = flutter.compileSdkVersion
-    // ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
